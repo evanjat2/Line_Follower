fa45850b81e3f0a5e52e888d89f38d3582e519b9
BetNares <72925939+BetNares@users.noreply.github.com>
2021-05-12 12:18:43 +0700

    popup 3-2-1 sebelum countup mulai

--- Line_follower/js/countdown.js
+++ Line_follower/js/countdown.js
@@ -9,6 +9,7 @@ let statSec = 59
 let statMil = 100
 
 function start() {
+	
     if (seconds != 239) stop()
     timer = setInterval(function() {
         if (seconds == 180) start2()
@@ -39,6 +40,19 @@ function start() {
             clearInterval(timer)
             pause()
         }
+		
+		if (seconds == 183) {
+				modal.style.display = "block";
+				popup()
+				span.onclick = function() {
+					modal.style.display = "none";
+				}
+				window.onclick = function(event) {
+					if (event.target == modal) {
+					modal.style.display = "none";
+					}
+				}
+		}
     },10)
 }
 
@@ -60,3 +74,51 @@ function cancel() {
     statSec = 59
     statMil = 100
 }
+
+
+
+// POP UP 1-2-3 SAAT COUNTDOWN MENIT KE 3 SEBELUM MULAI COUNTUP
+let countupTimer;
+
+function popup() {
+	let timeleft = 2;
+	if (seconds != 2) clearInterval(countupTimer)
+	countupTimer = setInterval(function(){
+			if(timeleft <= 0){
+				document.getElementById("tigadetik").innerHTML = "Countup started";
+				clearInterval(countupTimer);
+			} else {
+				document.getElementById("tigadetik").innerHTML = timeleft + " seconds left";
+			}
+			timeleft -= 1;
+			}, 1000);
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  popup()
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
